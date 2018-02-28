c63be493af6fea613b7cad42ccf52d455d8f70e0
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-02-28 15:34:19 +0100

    mise à jour des indentations.

--- src/main/java/service/PersonneDAO.java
+++ src/main/java/service/PersonneDAO.java
@@ -6,41 +6,40 @@ import jpa.EntityManagerHelper;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-public class PersonneDAO implements GenericDAO<Personne,Long>{
+public class PersonneDAO implements GenericDAO<Personne, Long> {
 
 
+  @Override
+  public Personne create(Personne personne) {
+    EntityTransaction t = EntityManagerHelper.getEntityManager().getTransaction();
+    t.begin();
+    EntityManagerHelper.getEntityManager().persist(personne);
+    t.commit();
+    return personne;
+  }
 
-    @Override
-    public Personne create(Personne personne) {
-       EntityTransaction t = EntityManagerHelper.getEntityManager().getTransaction();
-       t.begin();
-        EntityManagerHelper.getEntityManager().persist(personne);
-        t.commit();
-        return  personne;
-    }
+  @Override
+  public Personne read(Long id) {
 
-    @Override
-    public Personne read(Long id) {
+    return EntityManagerHelper.getEntityManager().find(Personne.class, id);
+  }
 
-        return  EntityManagerHelper.getEntityManager().find(Personne.class, id);
-    }
-
-    @Override
-    public Personne update(Personne personne) {
-        EntityTransaction t = EntityManagerHelper.getEntityManager().getTransaction();
-        t.begin();
-        EntityManagerHelper.getEntityManager().merge(personne);
-        t.commit();
-        return  personne;
-    }
+  @Override
+  public Personne update(Personne personne) {
+    EntityTransaction t = EntityManagerHelper.getEntityManager().getTransaction();
+    t.begin();
+    EntityManagerHelper.getEntityManager().merge(personne);
+    t.commit();
+    return personne;
+  }
 
 
-    @Override
-    public void delete(Personne personne) {
-        EntityTransaction t = EntityManagerHelper.getEntityManager().getTransaction();
-        t.begin();
-        EntityManagerHelper.getEntityManager().remove(personne);
-        t.commit();
+  @Override
+  public void delete(Personne personne) {
+    EntityTransaction t = EntityManagerHelper.getEntityManager().getTransaction();
+    t.begin();
+    EntityManagerHelper.getEntityManager().remove(personne);
+    t.commit();
 
-    }
+  }
 }
